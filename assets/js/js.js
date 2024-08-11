a4126aa9e120120d67267a8b3f60c86288913633
raimer aguilar <aguilar.raimer@gmail.com>
2024-08-11 14:41:44 -0400

    first commit

---
+++ assets/js/js.js
@@ -0,0 +1,18 @@
+
+let alertauno = document.querySelector('#uno')
+let alertados = document.querySelector('#dos')
+let alertatres= document.querySelector('#tres')
+let alertacuatro = document.querySelector('#cuatro')
+
+alertauno.addEventListener('click', () => {
+    alert("EN CONSTRUCCION");
+})
+alertados.addEventListener('click', () => {
+    alert("EN CONSTRUCCION");
+})
+alertatres.addEventListener('click', () => {
+    alert("EN CONSTRUCCION");
+})
+alertacuatro.addEventListener('click', () => {
+    alert("EN CONSTRUCCION");
+})
